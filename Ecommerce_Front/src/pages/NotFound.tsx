c60f6497fec2fcc50405910dc8d41e002d7fbc22
CJuan <cjuan.chona@hotmail.com>
2023-01-26 17:38:23 -0300

    Feature: se separo el router de react-router-dom asi como tambien la pagina de notfound

---
+++ Ecommerce_Front/src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+
+import '../static/css/NotFound.css'
+
+export default function NotFound() {
+  return (
+    <div className='NotFound'>
+        <h1  >404</h1>
+        <p>Page not Found</p>
+    </div>
+  )
+}
