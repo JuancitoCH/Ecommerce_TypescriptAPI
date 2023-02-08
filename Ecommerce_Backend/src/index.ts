fb4f67f00c13ff4965cf09b5fed9620c5256b138
CJuan <cjuan.chona@hotmail.com>
2023-02-08 17:58:30 -0300

    setup db connections

--- Ecommerce_Backend/src/index.ts
+++ Ecommerce_Backend/src/index.ts
@@ -1,6 +1,22 @@
 import app from "./app";
+import prisma from "./config/db";
 import envs from "./config/envs";
 
-app.listen(envs.PORT,()=>{
-    console.log('[💻 Server ]: Server Listen in Port '+envs.PORT)
+prisma.$connect()
+.then(()=>{
+    app.listen(envs.PORT,()=>{
+        console.log('[💻 Server ]: Server Listen in Port '+envs.PORT)
+    })
 })
+.catch(err=>{
+    console.error(err)
+    process.exit(1)
+})
+
+process.on("uncaughtException",async ()=>{
+    await prisma.$disconnect()
+})
+process.on("exit",async ()=>{
+    await prisma.$disconnect()
+})
+
